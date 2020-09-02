82c7d9b455f11e295ce93b17e0a8c0c50ac6b834
kakao6 <3103212161@qq.com>
2020-09-02 13:26:07 +0800

    Add files via upload

---
+++ 写着玩/写着玩/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  写着玩
+//
+//  Created by 魏跃勇 on 2020/8/26.
+//  Copyright © 2020 魏跃勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ 写着玩/写着玩/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  写着玩
+//
+//  Created by 魏跃勇 on 2020/8/26.
+//  Copyright © 2020 魏跃勇. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
